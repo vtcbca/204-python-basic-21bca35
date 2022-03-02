bd6d672d5090c4f361755d6b44c922c2edbaba7b
21bca35 <96609714+21bca35@users.noreply.github.com>
2022-03-02 10:55:22 +0530

    Add files via upload

---
+++ solution/reverse.py
@@ -0,0 +1,95 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("Enter any number:"))
+Enter any number:23
+>>> while(a>=0):
+	print(a)
+	a=a-1
+
+	
+23
+22
+21
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+>>> 
+==================== RESTART: V:/sem2/ps/script/script 1.py ====================
+Enter any number:23
+23
+22
+21
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+>>> 
+===================== RESTART: V:/sem2/ps/script/evenodd.py ====================
+Enter any number32
+Even number
+>>> 2
+2
+>>> 
+===================== RESTART: V:/sem2/ps/script/evenodd.py ====================
+Enter any number2
+Even number
+>>> 
+==================== RESTART: V:/sem2/ps/script/script 1.py ====================
+Enter any number:20
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+0
+>>> 
