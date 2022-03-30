0df97ef68ddc59164577a4579000b9ac9a76131f
21bca35 <96609714+21bca35@users.noreply.github.com>
2022-03-30 10:05:34 +0530

    Delete muliply.py

--- solution/muliply.py
+++
@@ -1,7 +0,0 @@
-#was to enter two number and print its multiplication using function
-def multiply():
-    a=int(input("Enter 1st value:"))
-    b=int(input("Enter 2nd value:"))
-    c=a*b
-    print("multiplication of two number is ".format(c))
-multiply()
